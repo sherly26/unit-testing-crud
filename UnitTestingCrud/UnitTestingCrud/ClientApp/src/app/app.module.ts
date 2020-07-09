e4fe229c53fa70b136c1d465716102bfaddc4518
Sherly26 <sherly.c.sanchez@gmail.com>
2020-07-08 21:00:10 -0400

    Commit 0: -Created net core web project with angular 10. -Created project structure. -Angular routing setted up. -Created shared module in angular. -Created home view.

---
+++ UnitTestingCrud/UnitTestingCrud/ClientApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from '../app/app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+    ]),
+    SharedModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
