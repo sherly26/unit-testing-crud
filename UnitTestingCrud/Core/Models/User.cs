a0739051edfc68ab12faf64116ff8093767534a2
Sherly26 <sherly.c.sanchez@gmail.com>
2020-07-09 16:23:19 -0400

    -Added user model.

---
+++ UnitTestingCrud/Core/Models/User.cs
@@ -0,0 +1,43 @@
+﻿namespace Core.Models
+{
+    /// <summary>
+    /// Represents User entity
+    /// </summary>
+    public sealed class User
+    {
+        /// <summary>
+        /// User name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// User lastname.
+        /// </summary>
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// Username.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// User phone
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// User address
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
